64f8cdb1266e358a94daf97dc98b3894f25a74df
shreyash patil <102778395+Shres0@users.noreply.github.com>
2023-11-01 15:11:44 +0530

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,7 +1,1 @@
-import webapp2
-class MainPage(webapp2.RequestHandler):
-  def get(self):
-    self.response.out.write("hello world")
-    self.response.out.write("I'm in GEngine")
-
-app=webapp2.WSGIApplication([('/',MainPage)], debug=True)
+print("hello")
